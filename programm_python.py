3e4f56efba0b1c26e9b0f4a56d8cd859d33bdb2e
10k20 <57794545+10k20@users.noreply.github.com>
2019-11-15 13:36:17 +0300

    Add files via upload

---
+++ programm_python.py
@@ -0,0 +1,11 @@
+#def common_member(a, b): 
+#    array_a = set(a) 
+#    array_b = set(b) 
+a = ["test" , "service2" , "dev" , "service4" , "test5" , "test6" , "test7" , "service8" , "prod"] 
+b = ["test6" , "service2" , "service4" , "service8"] 
+
+for i in a:
+	if i in b:
+		print(1, end='')
+	else: 
+		print(0, end='')
